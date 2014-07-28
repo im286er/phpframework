933f74d11d09f2c7e81792c9a2afbbe3029fee51
itziy <563268276@qq.com>
2014-07-28 10:43:51 +0800

    添加针对全局变量 /  /  的全局操作类

---
+++ framework/kernel/Get.class.php
@@ -0,0 +1,61 @@
+<?php
+/**
+* 全局变量，$_GET的操作类
+* @filename Get.class.php
+* @touch date 2014-07-28 10:24:14
+* @author Rain<563268276@qq.com>
+* @copyright 2014 http://www.94cto.com/
+* @license http://www.apache.org/licenses/LICENSE-2.0   LICENSE-2.0
+* @package Rain PHP Frame(RPF)
+*/
+
+/**
+* 全局变量，$_GET的操作类
+*/
+class Get
+{
+	/**
+	* 将值设置到$_GET中
+	* <code>Get::set('s_key', '123');</code>
+	* @param string $key  key
+	* @param string  $val value
+	* @return void 
+	*/
+	public static function set($key, $val)
+	{
+		$_GET[$key] = $val;
+	}
+
+	/**
+	* 获取$_GET中的值
+	* <code>Get::get('s_key');</code>
+	* @param string $key  key
+	* @return  string|bool 如果存在返回值，否则返回false
+	*/
+	public static function get($key)
+	{
+		return isset($_GET[$key]) ? $_GET[$key] : false;
+	}
+
+	/**
+	* 删除$_GET中的值
+	* <code>Get::rm('s_key');</code>
+	* @param string $key  key
+	* @return  void
+	*/
+	public static function rm($key)
+	{
+		unset($_GET[$key]);
+	}
+
+	/**
+	* 判断$_GET中的值是否设置
+	* <code>Get::exist('s_key');</code>
+	* @param string $key  key
+	* @return bool 成功返回true，失败返回false
+	*/
+	public static function exist($key)
+	{
+		return isset($_GET[$key]) ? true : false;
+	}
+}
